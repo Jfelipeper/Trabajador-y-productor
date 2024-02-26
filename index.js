061f5b57685f13a3d8e26ecf252d64113701083b
Jfelipeper <127033560+Jfelipeper@users.noreply.github.com>
2024-02-26 15:15:06 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const txtop1 =document.getElementById("op1");
+const txtop2 =document.getElementById("op2");
+const txtoperador =document.getElementById("operacion");
+const btnCalcular =document.getElementById("calcular");
+const pResultado=document.getElementById("resultado");
+
+
+function calcular(){
+    console.log("apretaste el boton calcular")
+}
